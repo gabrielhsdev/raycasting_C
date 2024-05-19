36c7780c21088b2ae0ba09ed43a3ce11d4c8845b
gabriel <henriquegabrielhs21@gmail.com>
2024-05-19 12:59:24 -0300

    Raycasting - Dev

--- globals.h
+++ globals.h
@@ -3,6 +3,7 @@
 
 #define MAP_WIDTH 8
 #define MAP_HEIGHT 8
+#define MAP_BLOCK_SIZE_PX 64 // Result of ( SCREEN_WIDTH / 2 ) / MAP_WIDTH
 #define SCREEN_WIDTH 1024
 #define SCREEN_HEIGHT 510
 #define MAP_BLOCK_WIDTH SCREEN_WIDTH / MAP_WIDTH / 2
@@ -22,6 +23,7 @@ typedef struct {
     float rotSpeed; // player rotation speed
 
     float ray_size; // ray size
+    float ray_angle; // ray angle
 } Player;
 
 // Create a player
@@ -31,9 +33,9 @@ Player player = {150, 400, 0, 0, -90, 5, 3, 100};
 int worldMap[MAP_WIDTH][MAP_HEIGHT] =
         {
                 1,1,1,1,1,1,1,1,
-                1,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,1,
+                1,0,1,0,0,0,0,1,
+                1,0,1,0,0,0,0,1,
+                1,0,1,0,0,0,0,1,
                 1,0,0,0,0,0,0,1,
                 1,0,0,0,0,0,0,1,
                 1,0,0,0,0,0,0,1,
---
+++ player/player.h
@@ -0,0 +1,8 @@
+//
+// Created by gabriel on 5/19/24.
+//
+
+#ifndef RAYCASTER_PLAYER_H
+#define RAYCASTER_PLAYER_H
+
+#endif //RAYCASTER_PLAYER_H
